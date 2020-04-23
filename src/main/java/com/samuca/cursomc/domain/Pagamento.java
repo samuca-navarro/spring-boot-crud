6b1d7bfd4b96d7ee338c13d34e5c9743c156dee0
unknown <samuca_navarro@outlook.com>
2020-04-22 21:36:33 -0300

    @JsonIgnore

--- src/main/java/com/samuca/cursomc/domain/Pagamento.java
+++ src/main/java/com/samuca/cursomc/domain/Pagamento.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.samuca.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -27,7 +27,7 @@ public abstract class Pagamento implements Serializable{
 	
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId  //serve para que o Id do pedido seja o mesmo que do pagamento 
